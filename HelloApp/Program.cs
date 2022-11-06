e84235ea9964cf68b7fb63e1651972712f8c0ae8
black <black@example.com>
2022-11-06 15:00:43 +0500

    hello everyone

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,14 +1,13 @@
-﻿using System;
-
+﻿
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello everyone");
         }
-
+        
         public static void GreetWhite()
         {
             Console.WriteLine("hello white");
